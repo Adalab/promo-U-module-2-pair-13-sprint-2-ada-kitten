62719ba60e0f1e09ad4a1386940c70e107adfd93
susanamorillo <susana.des@gmail.com>
2023-09-22 11:21:05 +0200

    search race and description

--- js/main.js
+++ js/main.js
@@ -1,46 +1,47 @@
-'use strict';
+"use strict";
 
 /* Elementos que usamos en el HTML */
-const newFormElement = document.querySelector('.js-new-form');
-const listElement = document.querySelector('.js-list');
-const searchButton = document.querySelector('.js-button-search');
-const buttonAdd = document.querySelector('.js-btn-add');
-const buttonCancelForm = document.querySelector('.js-btn-cancel');
-const inputDesc = document.querySelector('.js-input-desc');
-const inputPhoto = document.querySelector('.js-input-photo');
-const inputName = document.querySelector('.js-input-name');
-const inputRace = document.querySelector('.js-input-race');
+const newFormElement = document.querySelector(".js-new-form");
+const listElement = document.querySelector(".js-list");
+const searchButton = document.querySelector(".js-button-search");
+const buttonAdd = document.querySelector(".js-btn-add");
+const buttonCancelForm = document.querySelector(".js-btn-cancel");
+const inputDesc = document.querySelector(".js-input-desc");
+const inputPhoto = document.querySelector(".js-input-photo");
+const inputName = document.querySelector(".js-input-name");
+const inputRace = document.querySelector(".js-input-race");
 
-const linkNewFormElememt = document.querySelector('.js-button-new-form');
-const labelMessageError = document.querySelector('.js-label-error');
-const input_search_desc = document.querySelector('.js_in_search_desc');
+const linkNewFormElememt = document.querySelector(".js-button-new-form");
+const labelMessageError = document.querySelector(".js-label-error");
+const input_search_desc = document.querySelector(".js_in_search_desc");
+const input_search_race = document.querySelector(".js_in_search_race");
 
 //Objetos con cada gatito
 const kittenData_1 = {
-  image: 'https://dev.adalab.es/gato-siames.webp',
-  name: 'Anastacio',
-  desc: 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
-  race: 'Siamés',
+  image: "https://dev.adalab.es/gato-siames.webp",
+  name: "Anastacio",
+  desc: "Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.",
+  race: "Siamés",
 };
 const kittenData_2 = {
-  image: 'https://dev.adalab.es/sphynx-gato.webp',
-  name: 'Fiona',
-  desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
-  race: 'Sphynx',
+  image: "https://dev.adalab.es/sphynx-gato.webp",
+  name: "Fiona",
+  desc: "Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.",
+  race: "Sphynx",
 };
 const kittenData_3 = {
-  image: 'https://dev.adalab.es/maine-coon-cat.webp',
-  name: 'Sofia',
-  desc: ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
-  race: '',
+  image: "https://dev.adalab.es/maine-coon-cat.webp",
+  name: "Sofia",
+  desc: " Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.",
+  race: "",
 };
 
 const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
 
 //Funciones
 function renderKitten(kittenData) {
-  let html = '';
-  if (kittenData.race === '') {
+  let html = "";
+  if (kittenData.race === "") {
     html = `Uy que despiste, no sabemos su raza`;
   } else {
     html = kittenData.race;
@@ -64,7 +65,7 @@ function renderKitten(kittenData) {
 }
 
 function renderKittenList(kittenDataList) {
-  listElement.innerHTML = '';
+  listElement.innerHTML = "";
   for (const kittenItem of kittenDataList) {
     listElement.innerHTML += renderKitten(kittenItem);
   }
@@ -72,15 +73,15 @@ function renderKittenList(kittenDataList) {
 
 //Mostrar/ocultar el formulario
 function showNewCatForm() {
-  newFormElement.classList.remove('collapsed');
+  newFormElement.classList.remove("collapsed");
 }
 function hideNewCatForm() {
-  newFormElement.classList.add('collapsed');
+  newFormElement.classList.add("collapsed");
 }
 
 function handleClickNewCatForm(event) {
   event.preventDefault();
-  if (newFormElement.classList.contains('collapsed')) {
+  if (newFormElement.classList.contains("collapsed")) {
     showNewCatForm();
   } else {
     hideNewCatForm();
@@ -93,10 +94,10 @@ function addNewKitten(event) {
   const valueName = inputName.value;
   const valueDesc = inputDesc.value;
   const valueRace = inputRace.value;
-  if (valueDesc === '' || valuePhoto === '' || valueName === '') {
-    labelMessageError.innerHTML = '¡Uy! parece que has olvidado algo';
-  } else if (valueDesc !== '' && valuePhoto !== '' && valueName !== '') {
-    labelMessageError.innerHTML = '';
+  if (valueDesc === "" || valuePhoto === "" || valueName === "") {
+    labelMessageError.innerHTML = "¡Uy! parece que has olvidado algo";
+  } else if (valueDesc !== "" && valuePhoto !== "" && valueName !== "") {
+    labelMessageError.innerHTML = "";
 
     const newKittenDataObject = {
       image: valuePhoto,
@@ -109,42 +110,42 @@ function addNewKitten(event) {
     renderKittenList(kittenDataList);
 
     //Vaciar inputs al añadir gatito
-    inputDesc.value = '';
-    inputPhoto.value = '';
-    inputName.value = '';
-    inputRace.value = '';
-    labelMessageError.innerHTML = 'Mola! Un nuevo gatito en Adalab!';
+    inputDesc.value = "";
+    inputPhoto.value = "";
+    inputName.value = "";
+    inputRace.value = "";
+    labelMessageError.innerHTML = "Mola! Un nuevo gatito en Adalab!";
   }
 }
 
 //Cancelar la búsqueda de un gatito
 function cancelNewKitten(event) {
   event.preventDefault();
-  newFormElement.classList.add('collapsed');
-  inputDesc.value = '';
-  inputPhoto.value = '';
-  inputName.value = '';
-  inputRace.value = '';
-  labelMessageError.innerHTML = '';
+  newFormElement.classList.add("collapsed");
+  inputDesc.value = "";
+  inputPhoto.value = "";
+  inputName.value = "";
+  inputRace.value = "";
+  labelMessageError.innerHTML = "";
 }
 
 //Filtrar por descripción
 function filterKitten(event) {
   event.preventDefault();
   const descrSearchText = input_search_desc.value;
-  listElement.innerHTML = '';
-  for (const kittenItem of kittenDataList) {
-    if (kittenItem.desc.includes(descrSearchText)) {
-      listElement.innerHTML += renderKitten(kittenItem);
-    }
-  }
+  const descrSearchRace = input_search_race.value;
+  const kittenFilter = kittenDataList
+    .filter((kitten) => kitten.desc.includes(descrSearchText))
+    .filter((kitten) => kitten.race.includes(descrSearchRace));
+
+  renderKittenList(kittenFilter);
 }
 
 //Mostrar el litado de gatitos en el HTML
 renderKittenList(kittenDataList);
 
 //Eventos
-linkNewFormElememt.addEventListener('click', handleClickNewCatForm);
-searchButton.addEventListener('click', filterKitten);
-buttonAdd.addEventListener('click', addNewKitten);
-buttonCancelForm.addEventListener('click', cancelNewKitten);
+linkNewFormElememt.addEventListener("click", handleClickNewCatForm);
+searchButton.addEventListener("click", filterKitten);
+buttonAdd.addEventListener("click", addNewKitten);
+buttonCancelForm.addEventListener("click", cancelNewKitten);
